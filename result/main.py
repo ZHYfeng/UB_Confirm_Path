cca99f14f316b9cf323f4423a7e6a3e010dcc487
yhao <yhao016@ucr.edu>
2018-12-21 16:24:23 -0800

    add script

---
+++ result/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import sys
+import os.path
+
+def main():
+	filename = sys.argv[1]
+	file = open(filename)
+	line = file.readline()
+	while line:
+		line = line.replace("\n","")
+		cmd = "klee -json=\'"+line+"\' 2>&1 | tee result.txt"
+		
+		os.system(cmd)
+		# print(cmd)
+		line = file.readline()
+
+if __name__ =="__main__":
+	main()
