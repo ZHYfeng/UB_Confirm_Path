f810c05b473f90ef50abcfe20c2c2aa4918a5b95
yhao <yhao016@ucr.edu>
2019-01-03 14:40:06 -0800

    add usage

--- result/main_remote.py
+++ result/main_remote.py
@@ -8,7 +8,7 @@ def main():
 	line = file.readline()
 	while line:
 		line = line.replace("\n","")
-		cmd = "/home/yizhuo/2018_klee_confirm_path/build/bin/klee -json=\'"+line+"\' 2>&1 1>result.txt"
+		cmd = "/home/yizhuo/2018_klee_confirm_path/build/bin/klee -json=\'"+line+"\'"
 		
 		os.system(cmd)
 		# print(cmd)
